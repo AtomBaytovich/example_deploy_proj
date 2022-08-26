a3e662cd75415a20be3912445752bdcf3ae0b0ec
AtomBaytovich <atombaytovich@gmail.com>
2022-08-26 21:36:30 +0300

    edit readme and comment index

--- index.js
+++ index.js
@@ -6,18 +6,23 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// cors
 app.use(cors());
 app.options('*', cors());
 
+// шаблонизатор ejs
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public'))); // статическая папка
+// статическая папка
+app.use(express.static(path.join(__dirname, 'public'))); 
 
+// подключаем наши роуты
 app.use('/', require('./routes/index'));
 // Not Found
 app.use("*", (req, res) => res.send('404 | Not Found'));
 
+// прослушка
 app.listen(PORT, () => {
     console.log(`Проект запущен на http://localhost:${PORT}`);
 });
